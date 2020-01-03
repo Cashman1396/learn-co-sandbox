73f1f5c6efb9406103b715c443b0c074cf4158b0
Chris Ashman <chrisashman96@gmail.com>
2020-01-03 16:29:06 +0000

    Done.

---
+++ demo.rb
@@ -0,0 +1,13 @@
+case "The Mad Hatter"
+
+when "Alice"
+  puts "Hello Alice"
+when " The White Rabbit"
+  puts "Don't be late, White Rabbit"
+when "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+when "The Queen of Hearts"
+  puts "Please don't chop of my head!"
+else 
+  puts " Whooooo are you?"
+end
